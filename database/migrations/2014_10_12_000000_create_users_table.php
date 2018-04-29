da8e21bca818effd3cd52b2614a97293e3af36ff
Michael Rondon <mrondon72@gmail.com>
2018-04-29 14:35:07 -0400

    3. Formulario de busqueda y filtrado

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 98)->unique();
             $table->string('password');
             $table->text('bio');
             $table->rememberToken();
